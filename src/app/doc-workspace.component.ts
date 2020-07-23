bbaf4e84eaea6c8ada8d77ad3baff7162cfa1763
NeryaPe <67626832+NeryaPe@users.noreply.github.com>
2020-07-23 13:16:21 +0300

    add src

---
+++ src/app/doc-workspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-workspace',
+  templateUrl: './doc-workspace.component.html',
+  styleUrls: ['./doc-workspace.component.css']
+})
+export class DocWorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
